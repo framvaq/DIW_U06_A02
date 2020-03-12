9a841f0382f727b5584fc4a29600df4909fd091c
framvaq <francisco.ramirezvaquero@gmail.com>
2020-03-12 13:07:08 +0100

    faltan detallitos

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
     //VARIABLES
+    let id = "";
+    let pagina;
+    let pila = [];
 
     /*
 Lógica de la web
@@ -9,13 +12,18 @@ Al hacer click en la página de la foto, se "pasa" de página, y ocurre lo sigui
     - La página de la izquierda se oculta
     - La página hace la animación de pasarse
 
+PILA DE PÁGINAS:
+Al pasar la página, añado la página a la pila, en la primera posición
+Al ir hacia atrás, uso el método pop(), que me devuelve la última página en la pila, es decir, la que quiero borrar.
+
+
     */
 
     $('.pag').click(function () {
         // Si tiene la clase 'inicial' o 'anterior', hay que ir a la página siguiente. Si no,
         //ir a la anterior
-        ($(this).hasClass('inicial')) || ($(this).hasClass('anterior'))  ? 
-        pasarPagina(this) : retrodecerPagina(this);
+        ($(this).hasClass('inicial')) || ($(this).hasClass('anterior')) ?
+        pasarPagina(this): retrodecerPagina();
     });
 
     //Funcion pasar página
@@ -23,19 +31,44 @@ Al hacer click en la página de la foto, se "pasa" de página, y ocurre lo sigui
         console.log("pasar página");
         if ($(pagina).hasClass('anterior')) {
             $(pagina).removeClass('anterior');
-        } else if ($(pagina).hasClass('inicial')){
+
+        } else if ($(pagina).hasClass('inicial')) {
             $(pagina).removeClass('inicial');
         }
         //Avanzo una página
         $(pagina).addClass('siguiente');
+        //Meto la página en la pila
+        pila.push(pagina);
     }
 
-    function retrodecerPagina(pagina) {
+    function retrodecerPagina() {
         console.log("retroceder página");
+
+        //Obtengo la página que quiero, y la borro de la pila
+        let pagina = pila.pop();
+
+        //console.log (getId(pagina));
         if ($(pagina).hasClass('siguiente')) {
-            $(pagina).removeClass('siguiente')
+            $(pagina).removeClass('siguiente');
         }
+
         $(pagina).addClass('anterior');
 
+        if (pila.length == 0) {
+            $('#fondo').animate({
+                opacity: 0
+            }, 1000);
+        }
+
+    }
+
+    function getPagina(id) {
+        return pagina = $('#' + id)
+    }
+
+    function getId(imagen) {
+        id = $(imagen).attr('id');
+        console.log(id);
+        return id;
     }
 });
